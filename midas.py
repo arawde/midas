5001cac59ebc6ee36a7e546563521613c3ae833c
Avery Rawden <arawden@ondemandidaho.com>
2016-08-24 15:23:38 -0600

    Temporary fix for web service being borked

--- midas.py
+++ midas.py
@@ -11,8 +11,8 @@ def finance(symbol):
     returns:
     printed data
     '''
-
-    r = requests.get('https://finance.yahoo.com/webservice/v1/symbols/'+symbol+'/quote?format=json')
+    headers = {'user-agent' : 'Mozilla/5.0'}
+    r = requests.get('https://finance.yahoo.com/webservice/v1/symbols/'+symbol+'/quote?format=json', headers=headers)
     try:
         data = r.json()
         quote = data['list']['resources'][0]['resource']['fields']
